c45a9da005401f1e7d42d6b836902b1ce92dd541
Jose Tzec Marin <jose-tzec@MacBook-Air-de-Jose.local>
2024-03-27 19:34:12 -0600

    resize columns

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -5,7 +5,7 @@ interface GridProps {
 }
 
 const Grid: FC<GridProps> = ({ children }) => {
-  return <div className="grid grid-cols-3 gap-4 px-4 h-full">{children}</div>;
+  return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4 h-full">{children}</div>;
 };
 
 export default Grid;
